fb7eccd05f92c9df9d849b90cb900296de681210
LucaHaberli <luca.haberli@correo.ucu.edu.uy>
2025-09-22 11:13:11 -0300

    add: OR & test

--- Library.Tests/UnitTest1.cs
+++ Library.Tests/UnitTest1.cs
@@ -12,4 +12,14 @@ public class Tests
     {
         Assert.Pass();
     }
+    
+    [Test]
+    public void Flow2()
+    {
+        Input input1 = new Input(true);
+        Input input2 = new Input(false);
+        Input input3 = new Input(true);
+        Input resultado = input1.OR(input2); 
+        Console.WriteLine(resultado.bit);
+    }
 }
