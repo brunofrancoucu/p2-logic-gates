ca9f96e1e191b51fa67c25b3aaeffef6cee20b93
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2025-09-22 11:42:45 -0300

    add: GarageGate test

--- test/Library.Tests/UnitTest1.cs
+++ test/Library.Tests/UnitTest1.cs
@@ -7,18 +7,40 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-        
     }
 
     [Test]
-    public void Flow1()
+    public void AND()
     {
         Input a = new Input(true);
         Input b = new Input(false);
         Input c = new Input(false);
-        
-        a.AND(b).AND(c);    // method chaining
-        
+
+        a.AND(b).AND(c); // method chaining
+
         Assert.That(a.Bit, Is.EqualTo(true)); // prints: A AND B AND C
     }
+
+    [Test]
+    public void OR()
+    {
+        Input input1 = new Input(true);
+        Input input2 = new Input(false);
+        Input input3 = new Input(true);
+        
+        Input resultado = input1.OR(input2);
+        Console.WriteLine(resultado.Bit);
+    }
+
+    public void GarageTests()
+    {
+        GarageGate opened1 = new GarageGate(false, false, true);
+        GarageGate opened2 = new GarageGate(true, true, true);
+        GarageGate closed = new GarageGate(false, true, true);
+        
+        Assert.That(opened1.Evaluate(), Is.EqualTo(true)); // prints: A AND B AND C
+        Assert.That(opened2.Evaluate(), Is.EqualTo(true));
+        Assert.That(closed.Evaluate(), Is.EqualTo(false));
+    }
+
 }
