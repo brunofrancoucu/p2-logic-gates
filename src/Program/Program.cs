57db4808cfa951d138c40e31bcea557d4c21a780
bruno <main@brunos-MacBook-Pro.local>
2025-09-22 10:34:37 -0300

    add: Input constructor & NOT method

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,13 +2,22 @@
 
 class Input
 {
+    public string Value;
+    public bool Bit;
+
+    public Input(String value, bool bit)
+    {
+        Value = value;
+        Bit = bit;
+    }
     public Input AND(Input input2)
     {
     }
     public Input OR(Input input2)
     {
     }
-    public Input NOT(Input input2)
+    public Input NOT()
     {
+        return new Input(Value, !Bit);
     }
 }
--- test/Library.Tests/UnitTest1.cs
+++ test/Library.Tests/UnitTest1.cs
@@ -10,6 +10,13 @@ public class Tests
     [Test]
     public void Test1()
     {
+        Point a("A");
+        Point b("B");
+        Point c("C");
+        
+        a.AND(b).AND(c); // method chaining
+        a.show(); // prints: A AND B AND C
+    }
         Assert.Pass();
     }
 }
